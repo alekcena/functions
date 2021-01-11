06635b8b38b58271941bb93aa4f11535e7723cf0
Alekcena (Алексей) <53064004+alekcena@users.noreply.github.com>
2021-01-11 00:43:52 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+	function findDom(metod,selector){
+		if(typeof(metod) == 'string' && typeof(selector) == 'string'){
+
+		switch (metod){
+			case 'id':
+				return document.getElementById(selector);
+			case 'class':
+				return document.getElementsByClassName(selector);
+			case 'tag':
+				return document.getElementsByTagName(selector);
+			case 'name':
+				return document.getElementByName(selector);
+			}
+		}
+		else if(typeof(metod) == 'undefined' || typeof(selector) == 'undefined'){
+			console.log(`findDom('metod[id,class,tag,name]',selector)`);
+		}
+		else{
+			console.log("Error in type");
+		}
+	}
